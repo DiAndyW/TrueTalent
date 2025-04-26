cc08bf9fdb0709c265292b0e8b6b3e1e5c996d9d
Edwin-Yee <91923759+Edwin-Yee@users.noreply.github.com>
2025-04-25 22:28:45 -0700

    text and audio input work

--- gemini_live.py
+++ gemini_live.py
@@ -4,9 +4,10 @@
 
 
 import asyncio
-from google import genai
 import os
+import wave
 
+from google import genai
 from dotenv import load_dotenv
 
 load_dotenv()  
@@ -14,22 +15,50 @@ load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 model = "gemini-2.0-flash-live-001"
 
-config = {"response_modalities": ["TEXT"]}
+async def async_enumerate(aiterable, start=0):
+    index = start
+    async for item in aiterable:
+        yield index, item
+        index += 1
 
-async def main():
+async def process_audio(message):
+    config = {"response_modalities": ["AUDIO"]}
+
     async with client.aio.live.connect(model=model, config=config) as session:
-        while True:
-            message = input("User> ")
-            if message.lower() == "exit":
-                break
-            await session.send_client_content(
-                turns={"role": "user", "parts": [{"text": message}]}, turn_complete=True
-            )
+        wf = wave.open("audio.wav", "wb")
+        wf.setnchannels(1)
+        wf.setsampwidth(2)
+        wf.setframerate(24000)
 
-            async for response in session.receive():
-                if response.text is not None:
-                    print(response.text, end="")
+        await session.send_client_content(
+            turns={"role": "user", "parts": [{"text": str(message)}]}, turn_complete=True
+        )
+
+        async for idx,response in async_enumerate(session.receive()):
+            if response.data is not None:
+                wf.writeframes(response.data)
+
+        wf.close()
+
+
+async def process_text(msg):
+    config = {"response_modalities": ["TEXT"]}
+
+    async with client.aio.live.connect(model=model, config=config) as session:
+        await session.send_client_content(
+            turns={"role": "user", "parts": [{"text": msg}]}, turn_complete=True
+        )
+
+        response_text = ""
+        async for response in session.receive():
+            if response.text is not None:
+                response_text += response.text
+                print(response.text, end="")
+
+        print(f"\nGemini> {response_text}")
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(process_audio("Hey Gemini, how do you win a hackathon?"))
+    asyncio.run(process_text("Hey Gemini, how do you eat a burrito?"))
 
